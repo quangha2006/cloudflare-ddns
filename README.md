b4eaff408a24049084a5833eee5c7572125c8578
quangskyvu <quangskyvu@gmail.com>
2021-07-18 01:15:42 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,235 @@
+<p align="center"><a href="https://timknowsbest.com/free-dynamic-dns" target="_blank" rel="noopener noreferrer"><img width="1024" src="feature-graphic.jpg" alt="Cloudflare DDNS"/></a></p>
+
+# 🚀 Cloudflare DDNS
+
+Access your home network remotely via a custom domain name without a static IP!
+
+A small, 🕵️ privacy centric, and ⚡ lightning fast multi-architecture Docker image for self hosting projects.
+
+## 🇺🇸 Origin
+
+This script was written for the Raspberry Pi platform to enable low cost self hosting to promote a more decentralized internet.
+
+## 🧹 More than just DDNS
+
+`cloudflare-ddns` handles the busy work for you, so deploying web apps is less of a clickfest. Every 5 minutes, the script fetches public IPv4 and IPv6 addresses and then creates/updates DNS records for each subdomain in Cloudflare. Stale, duplicate DNS records are removed for housekeeping.
+
+## 📊 Stats
+
+| Size  | Downloads | Discord |
+| ------------- | ------------- | ------------- |
+| [![cloudflare-ddns docker image size](https://img.shields.io/docker/image-size/timothyjmiller/cloudflare-ddns?style=flat-square)](https://hub.docker.com/r/timothyjmiller/cloudflare-ddns "cloudflare-ddns docker image size")  | [![Total DockerHub pulls](https://img.shields.io/docker/pulls/timothyjmiller/cloudflare-ddns?style=flat-square)](https://hub.docker.com/r/timothyjmiller/cloudflare-ddns "Total DockerHub pulls")  | [![Official Discord Server](https://img.shields.io/discord/785778163887112192?style=flat-square)](https://discord.gg/UgGmwMvNxm "Official Discord Server")
+
+## ⁉️ How Private & Secure?
+
+1. Uses zero-log external IPv4 & IPv6 provider ([cdn-cgi/trace](https://www.cloudflare.com/cdn-cgi/trace))
+2. Alpine Linux base image
+3. HTTPS only via Python Software Foundation requests module
+4. Docker runtime
+5. Open source for open audits
+6. Regular updates
+
+## 🚦 Getting Started
+
+First copy the example configuration file into the real one.
+
+```bash
+cp config-example.json config.json
+```
+
+Edit `config.json` and replace the values with your own.
+
+### 🔑 Authentication methods
+
+You can choose to use either the newer API tokens, or the traditional API keys
+
+To generate a new API tokens, go to your [Cloudflare Profile](https://dash.cloudflare.com/profile/api-tokens) and create a token capable of **Edit DNS**. Then replace the value in
+
+```json
+"authentication":
+  "api_token": "Your cloudflare API token, including the capability of **Edit DNS**"
+```
+
+Alternatively, you can use the traditional API keys by setting appropriate values for:
+
+```json
+"authentication":
+  "api_key":
+    "api_key": "Your cloudflare API Key",
+    "account_email": "The email address you use to sign in to cloudflare",
+```
+
+### Enable or disable IPv4 or IPv6
+
+Some ISP provided modems only allow port forwarding over IPv4 or IPv6. In this case, you would want to disable any interface not accessible via port forward.
+
+```json
+"a": true,
+"aaaa": true
+```
+
+### Other values explained
+
+```json
+"zone_id": "The ID of the zone that will get the records. From your dashboard click into the zone. Under the overview tab, scroll down and the zone ID is listed in the right rail",
+"subdomains": "Array of subdomains you want to update the A & where applicable, AAAA records. IMPORTANT! Only write subdomain name. Do not include the base domain name. (e.g. foo or an empty string to update the base domain)",
+"proxied": false (defaults to false. Make it true if you want CDN/SSL benefits from cloudflare. This usually disables SSH)
+```
+
+## 📠 Hosting multiple subdomains on the same IP?
+
+You can save yourself some trouble when hosting multiple domains pointing to the same IP address (in the case of Traefik) by defining one A & AAAA record  'ddns.example.com' pointing to the IP of the server that will be updated by this DDNS script. For each subdomain, create a CNAME record pointing to 'ddns.example.com'. Now you don't have to manually modify the script config every time you add a new subdomain to your site!
+
+## 🌐 Hosting multiple domains (zones) on the same IP?
+
+You can handle ddns for multiple domains (cloudflare zones) using the same docker container by separating your configs inside ```config.json``` like below:
+
+```bash
+{
+  "cloudflare": [
+    {
+      "authentication": {
+          "api_token": "api_token_here", 
+          "api_key": {
+              "api_key": "api_key_here",
+              "account_email": "your_email_here"
+          }
+      },
+      "zone_id": "your_zone_id_here",
+      "subdomains": [
+        "",
+        "remove_or_replace_with_your_subdomain"
+      ],
+      "proxied": true
+    },
+    {
+      "authentication": {
+          "api_token": "api_token_here", 
+          "api_key": {
+              "api_key": "api_key_here",
+              "account_email": "your_email_here"
+          }
+      },
+      "zone_id": "your_zone_id_here",
+      "subdomains": [
+        "",
+        "remove_or_replace_with_your_subdomain"
+      ],
+      "proxied": true
+    }
+  ]
+}
+```
+
+## 🐳 Deploy with Docker Compose
+
+Pre-compiled images are available via [the official docker container on DockerHub](https://hub.docker.com/r/timothyjmiller/cloudflare-ddns).
+
+Modify the host file path of config.json inside the volumes section of docker-compose.yml.
+
+```yml
+version: "3.7"
+services:
+  cloudflare-ddns:
+    image: timothyjmiller/cloudflare-ddns:latest
+    container_name: cloudflare-ddns
+    security_opt:
+      - no-new-privileges:true
+    network_mode: "host"
+    environment:
+      - PUID=1000
+      - PGID=1000
+    volumes:
+      - /YOUR/PATH/HERE/config.json:/config.json
+    restart: unless-stopped
+```
+
+### ⚠️ IPv6
+
+Docker requires network_mode be set to host in order to access the IPv6 public address.
+
+### 🏃‍♂️ Running
+
+From the project root directory
+
+```bash
+docker-compose up -d
+```
+
+## 🐧 Deploy with Linux + Cron
+
+### 🏃 Running (all distros)
+
+This script requires Python 3.5+, which comes preinstalled on the latest version of Raspbian. Download/clone this repo and give permission to the project's bash script by running `chmod +x ./start-sync.sh`. Now you can execute `./start-sync.sh`, which will set up a virtualenv, pull in any dependencies, and fire the script.
+
+1. Upload the cloudflare-ddns folder to your home directory /home/your_username_here/
+
+2. Run the following code in terminal
+
+```bash
+crontab -e
+```
+
+3. Add the following lines to sync your DNS records every 15 minutes
+
+```bash
+*/15 * * * * /home/your_username_here/cloudflare-ddns/start-sync.sh
+```
+
+## Building from source
+
+Create a config.json file with your production credentials.
+
+### 💖 Please Note
+
+The optional `docker-build-all.sh` script requires Docker experimental support to be enabled.
+
+Docker Hub has experimental support for multi-architecture builds. Their official blog post specifies easy instructions for building with [Mac and Windows versions of Docker Desktop](https://docs.docker.com/docker-for-mac/multi-arch/).
+
+1. Choose build platform
+
+- Multi-architecture (experimental)  `docker-build-all.sh`
+
+- Linux/amd64 by default  `docker-build.sh`
+
+2. Give your bash script permission to execute.
+
+```bash
+sudo chmod +x ./docker-build.sh
+```
+
+```bash
+sudo chmod +x ./docker-build-all.sh
+```
+
+3. At project root, run the `docker-build.sh` script.
+
+Recommended for local development
+
+```bash
+./docker-build.sh
+```
+
+Recommended for production
+
+```bash
+./docker-build-all.sh
+```
+
+### Run the locally compiled version
+
+```bash
+docker run -d timothyjmiller/cloudflare_ddns:latest
+```
+
+## License
+
+This Template is licensed under the GNU General Public License, version 3 (GPLv3).
+
+## Author
+
+Timothy Miller
+
+[View my GitHub profile 💡](https://github.com/timothymiller)
+
+[View my personal website 💻](https://timknowsbest.com)
---
+++ cloudflare-ddns.py
@@ -0,0 +1,207 @@
+import requests, json, sys, signal, os, time, threading
+
+class GracefulExit:
+  def __init__(self):
+    self.kill_now = threading.Event()
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self, signum, frame):
+    print("🛑 Stopping main thread...")
+    self.kill_now.set()
+
+def deleteEntries(type):
+    # Helper function for deleting A or AAAA records
+    # in the case of no IPv4 or IPv6 connection, yet
+    # existing A or AAAA records are found.
+    for option in config["cloudflare"]:
+        answer = cf_api(
+            "zones/" + option['zone_id'] + "/dns_records?per_page=100&type=" + type,
+            "GET", option)
+    if answer is None or answer["result"] is None:
+        time.sleep(5)
+        return
+    for record in answer["result"]:
+        identifier = str(record["id"])
+        cf_api(
+            "zones/" + option['zone_id'] + "/dns_records/" + identifier, 
+            "DELETE", option)
+        print("🗑️ Deleted stale record " + identifier)
+
+def getIPs():
+    a = None
+    aaaa = None
+    global ipv4_enabled
+    global ipv6_enabled
+    if ipv4_enabled:
+        try:
+            a = requests.get("https://1.1.1.1/cdn-cgi/trace").text.split("\n")
+            a.pop()
+            a = dict(s.split("=") for s in a)["ip"]
+        except Exception:
+            global shown_ipv4_warning
+            if not shown_ipv4_warning:
+                shown_ipv4_warning = True
+                print("🧩 IPv4 not detected")
+            deleteEntries("A")
+    if ipv6_enabled:
+        try:
+            aaaa = requests.get("https://[2606:4700:4700::1111]/cdn-cgi/trace").text.split("\n")
+            aaaa.pop()
+            aaaa = dict(s.split("=") for s in aaaa)["ip"]
+        except Exception:
+            global shown_ipv6_warning
+            if not shown_ipv6_warning:
+                shown_ipv6_warning = True
+                print("🧩 IPv6 not detected")
+            deleteEntries("AAAA")
+    ips = {}
+    if(a is not None):
+        ips["ipv4"] = {
+            "type": "A",
+            "ip": a
+        }
+    if(aaaa is not None):
+        ips["ipv6"] = {
+            "type": "AAAA",
+            "ip": aaaa
+        }
+    return ips
+
+def commitRecord(ip):
+    for option in config["cloudflare"]:
+        subdomains = option["subdomains"]
+        response = cf_api("zones/" + option['zone_id'], "GET", option)
+        if response is None or response["result"]["name"] is None:
+            time.sleep(5)
+            return
+        base_domain_name = response["result"]["name"]
+        ttl = 300 # default Cloudflare TTL
+        for subdomain in subdomains:
+            subdomain = subdomain.lower().strip()
+            record = {
+                "type": ip["type"],
+                "name": subdomain,
+                "content": ip["ip"],
+                "proxied": option["proxied"],
+                "ttl": ttl
+            }
+            dns_records = cf_api(
+                "zones/" + option['zone_id'] + "/dns_records?per_page=100&type=" + ip["type"], 
+                "GET", option)
+            fqdn = base_domain_name
+            if subdomain:
+                fqdn = subdomain + "." + base_domain_name
+            identifier = None
+            modified = False
+            duplicate_ids = []
+            if dns_records is not None:
+                for r in dns_records["result"]:
+                    if (r["name"] == fqdn):
+                        if identifier:
+                            if r["content"] == ip["ip"]:
+                                duplicate_ids.append(identifier)
+                                identifier = r["id"]
+                            else:
+                                duplicate_ids.append(r["id"])
+                        else:
+                            identifier = r["id"]
+                            if r['content'] != record['content'] or r['proxied'] != record['proxied']:
+                                modified = True
+            if identifier:
+                if modified:
+                    print("📡 Updating record " + str(record))
+                    response = cf_api(
+                        "zones/" + option['zone_id'] + "/dns_records/" + identifier,
+                        "PUT", option, {}, record)
+            else:
+                print("➕ Adding new record " + str(record))
+                response = cf_api(
+                    "zones/" + option['zone_id'] + "/dns_records", "POST", option, {}, record)
+            for identifier in duplicate_ids:
+                identifier = str(identifier)
+                print("🗑️ Deleting stale record " + identifier)
+                response = cf_api(
+                    "zones/" + option['zone_id'] + "/dns_records/" + identifier,
+                    "DELETE", option)
+    return True
+
+def cf_api(endpoint, method, config, headers={}, data=False):
+    api_token = config['authentication']['api_token']
+    if api_token != '' and api_token != 'api_token_here':
+        headers = {
+            "Authorization": "Bearer " + api_token,
+            **headers
+        }
+    else:
+        headers = {
+            "X-Auth-Email": config['authentication']['api_key']['account_email'],
+            "X-Auth-Key": config['authentication']['api_key']['api_key'],
+        }
+
+    if(data == False):
+        response = requests.request(
+            method, "https://api.cloudflare.com/client/v4/" + endpoint, headers=headers)
+    else:
+        response = requests.request(
+            method, "https://api.cloudflare.com/client/v4/" + endpoint,
+            headers=headers, json=data)
+
+    if response.ok:
+        return response.json()
+    else:
+        print("📈 Error sending '" + method + "' request to '" + response.url + "':")
+        print(response.text)
+        return None
+
+def updateIPs(ips):
+    for ip in ips.values():
+        commitRecord(ip)
+
+if __name__ == '__main__':
+    PATH = os.getcwd() + "/"
+    version = float(str(sys.version_info[0]) + "." + str(sys.version_info[1]))
+    shown_ipv4_warning = False
+    shown_ipv6_warning = False
+    ipv4_enabled = True
+    ipv6_enabled = True
+
+    if(version < 3.5):
+        raise Exception("🐍 This script requires Python 3.5+")
+
+    config = None
+    try:
+        with open(PATH + "config.json") as config_file:
+            config = json.loads(config_file.read())
+    except:
+        print("😡 Error reading config.json")
+        time.sleep(60) # wait 60 seconds to prevent excessive logging on docker auto restart
+
+    if config is not None:
+        try:
+            ipv4_enabled = config["a"]
+            ipv6_enabled = config["aaaa"]
+        except:
+            ipv4_enabled = True
+            ipv6_enabled = True
+            print("⚙️ Individually disable IPv4 or IPv6 with new config.json options. Read more about it here: https://github.com/timothymiller/cloudflare-ddns/blob/master/README.md")
+        if(len(sys.argv) > 1):
+            if(sys.argv[1] == "--repeat"):
+                delay = 5*60
+                if ipv4_enabled and ipv6_enabled:
+                    print("🕰️ Updating IPv4 (A) & IPv6 (AAAA) records every 5 minutes")
+                elif ipv4_enabled and not ipv6_enabled:
+                    print("🕰️ Updating IPv4 (A) records every 5 minutes")
+                elif ipv6_enabled and not ipv4_enabled:
+                    print("🕰️ Updating IPv6 (AAAA) records every 5 minutes")
+                next_time = time.time()
+                killer = GracefulExit()
+                prev_ips = None
+                while True:
+                    if killer.kill_now.wait(delay):
+                        break
+                    updateIPs(getIPs())
+            else:
+                print("❓ Unrecognized parameter '" + sys.argv[1] + "'. Stopping now.")
+        else:
+            updateIPs(getIPs())
